2ac148e9bb61563d52378f299086ac7a8f473701
Carlos Martí <carlosmartichafer97@gmail.com>
2022-03-25 19:13:01 +0100

    env added

--- src/services/Api/Api.js
+++ src/services/Api/Api.js
@@ -4,7 +4,7 @@ const CORE_CONTEXT_PATH = "/private/core/v1";
 const CRYPTO_CONTEXT_PATH = "/private/crypto/v1";
 
 const axiosAPI = axios.create({
-  baseURL : "http://localhost:8029"
+  baseURL : "https://cryptoboard.es"
 });
 
 const apiRequest = (method, url, request) => {
